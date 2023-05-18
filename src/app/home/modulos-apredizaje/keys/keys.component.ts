9730c3845df0bfb50966270984693a2e02979989
Angello Gaitan Lopez <angello29@utp.edu.co>
2023-05-17 22:21:16 -0500

    dragmulti

--- src/app/home/modulos-apredizaje/keys/keys.component.ts
+++ src/app/home/modulos-apredizaje/keys/keys.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-keys',
   templateUrl: './keys.component.html',
   styleUrls: ['./keys.component.css']
 })
-export class KeysComponent {
+export class KeysComponent implements OnInit {
 
+  public listNumbers1;
+  public listNumbers2;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.listNumbers1 = ['CREATE', 'CONNECT', 'USER', 'WHERE', 'JOIN', 'NEW', 'SELECT', 
+                          'FROM','RIGHT JOIN', 'INSERT', 'UPDATE', 'INNER JOIN', 'TABLE', 'COLUMN', 'DROP', 
+                          'LEFT JOIN', 'INTO', '(',')', 'GROUP', 'BY', 'HAVING', 'ORDER', ',', 
+                          'PRIMARY KEY', 'FOREING KEY','nombre_de_la_tabla',';',':','columna1','tipo_de_dato','condiciones'];
+    this.listNumbers2 = [];
+  }
+
+  drop($event: CdkDragDrop<number[]>) {
+
+    if ($event.previousContainer === $event.container) {
+      moveItemInArray(
+        $event.container.data,
+        $event.previousIndex,
+        $event.currentIndex
+      )
+    } else {
+      transferArrayItem(
+        $event.previousContainer.data,
+        $event.container.data,
+        $event.previousIndex,
+        $event.currentIndex
+      );
+    }
+  }
 }
--- src/app/home/modulos-apredizaje/tipos-datos/tipos-datos.component.ts
+++ src/app/home/modulos-apredizaje/tipos-datos/tipos-datos.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-tipos-datos',
   templateUrl: './tipos-datos.component.html',
   styleUrls: ['./tipos-datos.component.css']
 })
-export class TiposDatosComponent {
+export class TiposDatosComponent implements OnInit {
 
+  public listNumbers1;
+  public listNumbers2;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.listNumbers1 = ['CREATE', 'INT', 'VARCHAR(tamaño)', 'WHERE', 'JOIN', 'NEW', 'SELECT', 
+                          'FROM','RIGHT JOIN', 'INSERT', 'UPDATE', 'INNER JOIN', 'TABLE', 'COLUMN', 'DATE', 
+                          'LEFT JOIN', 'INTO', '(',')', 'GROUP', 'BY', 'DECIMAL(precision, escala)', 'ORDER', ',', 
+                          'PRIMARY KEY', 'FOREING KEY','nombre_de_la_tabla',';',':','columna1','tipo_de_dato','condiciones'];
+    this.listNumbers2 = [];
+  }
+
+  drop($event: CdkDragDrop<number[]>) {
+
+    if ($event.previousContainer === $event.container) {
+      moveItemInArray(
+        $event.container.data,
+        $event.previousIndex,
+        $event.currentIndex
+      )
+    } else {
+      transferArrayItem(
+        $event.previousContainer.data,
+        $event.container.data,
+        $event.previousIndex,
+        $event.currentIndex
+      );
+    }
+  }
 }
